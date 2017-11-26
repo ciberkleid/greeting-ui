db08dbd3f6f5460085c07ca6b756493708a2ff69
Cora Iberkleid <ciberkleid@pivotal.io>
2017-11-26 00:49:14 -0500

    remove unused imports

--- src/main/java/io/pivotal/fortune/FortuneService.java
+++ src/main/java/io/pivotal/fortune/FortuneService.java
@@ -2,10 +2,7 @@ package io.pivotal.fortune;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
