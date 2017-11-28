3ba31e40a5426f43a485598156f80fbf4ffdf4aa
Cora Iberkleid <ciberkleid@pivotal.io>
2017-11-28 07:39:34 -0500

    added logging

--- src/test/java/e2e/E2eTests.java
+++ src/test/java/e2e/E2eTests.java
@@ -1,19 +1,16 @@
 package e2e;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
-
-import java.lang.invoke.MethodHandles;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author Marcin Grzejszczak
@@ -24,6 +21,9 @@ import java.util.concurrent.TimeUnit;
 @EnableAutoConfiguration
 public class E2eTests {
 
+	Logger logger = LoggerFactory
+			.getLogger(E2eTests.class);
+
 	//	hit /, check that 2xx and body does NOT contain “This fortune is no good. Try another.”
 	// also does NOT contain “The fortuneteller will be back soon.”
 
@@ -32,10 +32,11 @@ public class E2eTests {
 	RestTemplate restTemplate = new RestTemplate();
 
 	@Test
-	public void should_return_a_foo_fortune() {
+	public void should_return_a_fortune() {
 		ResponseEntity<String> response = this.restTemplate
 				.getForEntity("http://" + this.applicationUrl + "/", String.class);
 
+		logger.info("Repsonse: [{}]", response);
 		BDDAssertions.then(response.getStatusCodeValue()).isEqualTo(200);
 		BDDAssertions.then(response.getBody()).doesNotContain("The fortuneteller will be back soon.").doesNotContain("This fortune is no good. Try another.");
 	}
