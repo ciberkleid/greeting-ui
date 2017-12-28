bbf47aada95a997dafe00693334b6f65c1e9b678
Cora Iberkleid <ciberkleid@gmail.com>
2017-12-28 13:02:23 -0500

    Update after upgrading stubrunner to spring cloud Edgware

--- src/main/java/io/pivotal/fortune/FortuneService.java
+++ src/main/java/io/pivotal/fortune/FortuneService.java
@@ -1,30 +1,26 @@
 package io.pivotal.fortune;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Component
 public class FortuneService {
 
   Logger logger = LoggerFactory
-      .getLogger(FortuneService.class);
+          .getLogger(FortuneService.class);
 
-  private final String fortuneServiceUrl;
   private final RestTemplate restTemplate;
 
-  public FortuneService(RestTemplate restTemplate, @Value("${fortune.service.url:fortune-service}") String fortuneServiceUrl) {
+  public FortuneService(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
-    this.fortuneServiceUrl = fortuneServiceUrl;
   }
 
   @HystrixCommand(fallbackMethod = "defaultFortune")
   public String getFortune() {
-    String fortune = restTemplate.getForObject("http://".concat(fortuneServiceUrl), String.class);
+    String fortune = restTemplate.getForObject("http://fortune-service", String.class);
     return fortune;
   }
 
