e137d041df74093e06313546647cc32f9570abba
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-28 08:43:16 -0500

    optimized imports

--- src/test/java/io/pivotal/GreetingHystrixApplicationTests.java
+++ src/test/java/io/pivotal/GreetingHystrixApplicationTests.java
@@ -5,8 +5,6 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GreetingHystrixApplication.class)
 public class GreetingHystrixApplicationTests {
