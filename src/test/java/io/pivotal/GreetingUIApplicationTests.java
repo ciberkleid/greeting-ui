4e72c28a3062eee2858e5ab79c247daf9eb55ae8
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-31 10:09:55 -0500

    adding test from stage2

---
+++ src/test/java/io/pivotal/GreetingUIApplicationTests.java
@@ -0,0 +1,17 @@
+package io.pivotal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = GreetingUIApplication.class)
+public class GreetingUIApplicationTests {
+
+    @Test
+    public void contextLoads() throws Exception {
+
+    }
+
+}
