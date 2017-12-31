5bfb4b3de42b29c4adbf57d397887f7f19db1ba8
Cora Iberkleid <ciberkleid@pivotal.io>
2017-12-31 10:04:08 -0500

    renamed main class name to match project name

--- src/test/java/io/pivotal/GreetingUIApplicationTests.java
+++ src/test/java/io/pivotal/GreetingUIApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = GreetingHystrixApplication.class)
-public class GreetingHystrixApplicationTests {
+@SpringBootTest(classes = GreetingUIApplication.class)
+public class GreetingUIApplicationTests {
 
     @Test
     public void contextLoads() throws Exception {
