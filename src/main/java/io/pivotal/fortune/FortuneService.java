b081ae55f781b096e35a61ff59229e4ceb5b9264
Cora Iberkleid <ciberkleid@pivotal.io>
2018-10-01 02:20:51 -0400

    upgraded spring

--- src/main/java/io/pivotal/fortune/FortuneService.java
+++ src/main/java/io/pivotal/fortune/FortuneService.java
@@ -3,6 +3,7 @@ package io.pivotal.fortune;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,18 +15,22 @@ public class FortuneService {
 
   private final RestTemplate restTemplate;
 
+  @Value("${fortuneServiceURL:http://fortune-service}")
+  String fortuneServiceURL;
+
   public FortuneService(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
   }
 
   @HystrixCommand(fallbackMethod = "defaultFortune")
   public String getFortune() {
-    String fortune = restTemplate.getForObject("http://fortune-service", String.class);
+    logger.debug("Using fortuneServiceURL=[{}]", fortuneServiceURL);
+    String fortune = restTemplate.getForObject(fortuneServiceURL, String.class);
     return fortune;
   }
 
-  public String defaultFortune(){
-    logger.debug("Default fortune used.");
+  public String defaultFortune(Throwable throwable){
+    logger.debug("Returning fallback fortune. Error: {}", throwable.getMessage());
     return "This fortune is no good. Try another.";
   }
 
