e7ee3b5784f057c74b2ef1b24882cf2343b7eb78
Cora Iberkleid <ciberkleid@pivotal.io>
2019-04-04 11:57:32 -0400

    use http for build, parameterize stub id

--- src/test/java/io/pivotal/fortune/FortuneServiceTests.java
+++ src/test/java/io/pivotal/fortune/FortuneServiceTests.java
@@ -7,16 +7,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GreetingUIApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE,
-        properties = {"spring.application.name=greeting-ui", "spring.cloud.circuit.breaker.enabled=false", "hystrix.stream.queue.enabled=false"})
-@AutoConfigureStubRunner(ids = {"io.pivotal:fortune-service:1.0.0.M1-20180725_221358-VERSION"},
+        properties = {"spring.application.name=greeting-ui", "fortuneServiceURL=http://fortune-service", "spring.cloud.circuit.breaker.enabled=false", "hystrix.stream.queue.enabled=false"})
+//@AutoConfigureStubRunner(ids = {"io.pivotal:fortune-service:10-78119a9bbde61510640ffc93beed4697e07f4ef0"},
 //@AutoConfigureStubRunner(ids = {"io.pivotal:fortune-service:+"},
-        repositoryRoot = "${REPO_WITH_BINARIES}"
+@AutoConfigureStubRunner(ids = {"${STUBRUNNER_IDS_FORTUNE_SERVICE}"},
+        repositoryRoot = "${REPO_WITH_BINARIES}",
+        stubsMode = StubRunnerProperties.StubsMode.REMOTE
         //workOffline = true
-
 )
 
 public class FortuneServiceTests {
